c9b579f1fdeb032852918da532d6afe6296c3a00
Shuvkant <weird426344@gmail.com>
2025-03-04 10:43:14 +0545

    removing the nodemailer credentials

--- backend_mail/app.py
+++ backend_mail/app.py
@@ -11,8 +11,8 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-EMAIL_USER = 'strange426344@gmail.com'
-EMAIL_PASS = 'nsqhrjstugtxfjwb'
+EMAIL_USER = ''
+EMAIL_PASS = ''
 
 
 @app.route('/send-email', methods=['POST'])
