3f27059ccc11a630d1433fcdb60f2f06e5015bd7
Shuvkant <weird426344@gmail.com>
2025-03-10 12:38:11 +0545

    removing security credentials

--- frontend/src/integrations/supabase/client.ts
+++ frontend/src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://ndnjhrwapmkdoxbwzcaf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kbmpocndhcG1rZG94Ynd6Y2FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NjE1NzEsImV4cCI6MjA1NjQzNzU3MX0.pbSGTKGtRG8cymrhilIDGahVy3NIiN8wVLeSnmcsOoE";
+const SUPABASE_PUBLISHABLE_KEY = "";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
